929addc29056135f16585ae0336523bd6a7f1f74
Mike Rhoden <mike.rhoden.dev@gmail.com>
2021-10-04 16:12:08 -0500

    first commit

---
+++ CpInterview.Tests/CalendarInteractorTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CpInterview.Tests
+{
+  [TestClass]
+  public class CalendarInteractorTests
+  {
+    [TestMethod]
+    public void CanCreateCalendarInteractor()
+    {
+      var calendarInteractor = new CalendarInteractor();
+
+    }
+  }
+}
---
+++ CpInterview/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CpInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
